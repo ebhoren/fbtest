a6adb0c30ebe6af22931d4ebe165929981b5a563
Dominic Mercier <dominic@dominic-mercier.com>
2016-12-22 11:47:11 -0500

    first commit

---
+++ www/js/index.js
@@ -0,0 +1,91 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+
+    rootEl: null,
+
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        this.rootEl = document.getElementById(id);
+
+        var listeningElement = this.rootEl.querySelector('.listening');
+        var receivedElement = this.rootEl.querySelector('.received');
+        var facebookElement = this.rootEl.querySelector('.facebook');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+        facebookElement.setAttribute('style', 'display:block;');
+
+        facebookElement.addEventListener('click', this.onFacebook, false);
+
+        console.log('Received Event: ' + id);
+    },
+    // Trigger Facebook modal window
+    onFacebook: function(e) {
+        e.preventDefault();
+
+        window.plugins.socialsharing.canShareVia(null, 'Hello World', null, null, null, app.shareCallback, app.shareCallback);
+        //window.plugins.socialsharing.share('Hello World');
+        //window.plugins.socialsharing.shareVia('com.facebook.katana', 'Hello World 2');
+
+        return false;
+    },
+    shareCallback: function(e) {
+
+        console.log(e);
+
+        var container = document.getElementById('applications');
+        var apps = e.toString().split(',').filter(function(application) { return application.indexOf('facebook') > -1; });
+        var output = '';
+        
+        for(var i = 0, n = apps.length; i < n; i++) {
+            output += '<a href="#" class="event btn" data-app="'+apps[i]+'">Share to '+apps[i]+'</a>'
+        }
+
+        container.addEventListener('click', app.share, false);
+        container.innerHTML = output;
+    },
+    share: function(e) {
+
+        e.preventDefault();
+
+        var app = e.target.dataset.app;
+        if( !app ) return false;
+        
+        window.plugins.socialsharing.shareVia(app, 'Hello World Message', 'Hello World Subject', null, 'http://www.google.com');
+    }
+};
